b56bfdd1860ccde665ee071eb14546f5476d7bef
CHOLLET Axel <cholletax@orange.fr>
2015-07-30 09:57:08 +0200

    Ajout input type range modificateur largeur du canvas

--- index.php
+++ index.php
@@ -25,29 +25,36 @@
             
             <div class = "groupes">
                 
-                <label for = 'vitesseX'>Vélocité en X : (min : -30 max : 30 default : 15)</label>
-                <input id = 'vitesseX' type = 'number' step = '1' min = "-30" max = "30" value = "15"/> 
+                <label for = 'vitesseXInput'>Vélocité en X (min : -30 max : 30 default : 15) :</label>
+                <input id = 'vitesseXInput' type = 'number' step = '1' min = "-30" max = "30" value = "15"/> 
             
             </div>
             
             <div class = "groupes">
                 
-                <label for = 'vitesseY'>Vélocité en Y : (min : -30 max : 30 default : 15)</label>
-                <input id = 'vitesseY' type = 'number' step = '1' min = "-30" max = "30" value = "15"/> 
-            
-            </div>
-
-            <div class = "groupes">
-                
-                <label for = 'gravity'>Gravité : (min : 0.1 max : 0.9 default : 0.8)</label>
-                <input id = 'gravity' type = 'number' step = '0.1' min = "0" max = "1" value = "0.8"/> 
+                <label for = 'vitesseYInput'>Vélocité en Y (min : -30 max : 30 default : 15) :</label>
+                <input id = 'vitesseYInput' type = 'number' step = '1' min = "-30" max = "30" value = "15"/> 
             
             </div>
 
             <div class = "groupes">
                 
-                <label for = 'taille'>Rayon de la balle : (min : 1 max : 30 default : 20)</label>
-                <input id = 'taille' type = 'number' step = '1' min = "1" max = "30" value = "20"/> 
+                <label for = 'gravityInput'>Gravité (min  0.1 max : 0.9 default : 0.8) :</label>
+                <input id = 'gravityInput' type = 'number' step = '0.1' min = "0" max = "1" value = "0.8"/> 
+            
+            </div>
+
+            <div class = "groupes">
+                
+                <label for = 'tailleInput'>Rayon de la balle (min : 1 max : 30 default : 20) :</label>
+                <input id = 'tailleInput' type = 'number' step = '1' min = "1" max = "30" value = "20"/> 
+            
+            </div>
+
+            <div class = "groupes">
+                
+                <label for = 'largeurInput'>Largeur du canvas (min : 1 max : 2.5 default : 1.1) :</label>
+                <input id = 'largeurInput' type = 'range' step = '0.01' min = "1" max = "2.5" value = "1.1"/> 
             
             </div>
 
